c189cbd23232df6ceedc2c2f75ee769ffe4e691c
22288 <josiah.lugo@vtsd.com>
2025-12-11 11:03:33 -0500

    Repository #1

--- calc/calc.js
+++ calc/calc.js
@@ -10,3 +10,29 @@ buttons.forEach(
         button.addEventListener("click", calculate);
     }
 );
+
+// Perform the calculations on our calculator
+function calculate(event){
+    // What was clicked?
+    const clickedValue = event.target.value;
+
+    // Handle what was clicked
+    // If it's an equal sign
+    if(clickedValue === "="){
+        // Is the display empty?
+        if(display.value !== ""){
+            // Calculate the value and display
+            display.value = eval(display.value);
+        }
+    }
+    // Did I click clear?
+    else if(clickedValue === "C"){
+        // Empty the Display
+        display.value = "";
+    }
+    // Otherwise add it to the display
+    else{
+        // Add whatever I clicked to the display
+        display.value += clickedValue;
+    }
+}
